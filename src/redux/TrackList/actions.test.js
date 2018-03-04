c7975365466c91763542b1a322d0dad66002ef44
georgesbiaux <georgesb@theodo.fr>
2018-03-04 09:06:54 +0100

    Test TrackList actions

---
+++ src/redux/TrackList/actions.test.js
@@ -0,0 +1,26 @@
+import { TOGGLE_TRACK_PRIORITY, TOGGLE_USER_VOTE } from './constants';
+import { toggleTrackPriority, toggleUserVote } from './actions';
+
+describe('TrackList actions', () => {
+  describe('toggleTrackPriority', () => {
+    it('should return the correct type and payload', () => {
+      const expectedResult = {
+        type: TOGGLE_TRACK_PRIORITY,
+        trackId: 42,
+      };
+
+      expect(toggleTrackPriority(42)).toEqual(expectedResult);
+    });
+  });
+
+  describe('toggleUserVote', () => {
+    it('should return the correct type and payload', () => {
+      const expectedResult = {
+        type: TOGGLE_USER_VOTE,
+        trackId: 42,
+      };
+
+      expect(toggleUserVote(42)).toEqual(expectedResult);
+    });
+  });
+});
